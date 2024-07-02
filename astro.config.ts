f6e36457c7dbd672ca95adcf04426856390c9432
Lukasz <lukasz.zatyka@gmail.com>
2024-07-02 21:16:59 +0200

    Cleaning code

--- astro.config.ts
+++ astro.config.ts
@@ -13,7 +13,7 @@ dotenv.config();
 // https://astro.build/config
 export default defineConfig({
 	site: "https://zetkolek.netlify.app/",
-	trailingSlash: 'always',
+	trailingSlash: "always",
 	integrations: [
 		expressiveCode(expressiveCodeOptions),
 		icon({
--- src/pages/api/auth/signout.ts
+++ src/pages/api/auth/signout.ts
@@ -1,8 +1,8 @@
 import type { APIRoute } from "astro";
 
 export const GET: APIRoute = async ({ redirect, cookies }) => {
-  cookies.delete("__session", {
-    path: "/",
-  });
-  return redirect("/signin");
+	cookies.delete("__session", {
+		path: "/",
+	});
+	return redirect("/signin");
 };
