7d89f2328cb1287a362c91eb98620a18915712ea
Lukasz <lukasz.zatyka@gmail.com>
2024-07-05 12:57:11 +0200

    Enhanced PasswordReset with card layout and navigation button

--- src/components/portfolio/firebase-auth/PasswordReset.component.tsx
+++ src/components/portfolio/firebase-auth/PasswordReset.component.tsx
@@ -6,6 +6,8 @@ import { Button } from "@/components/ui/button";
 import * as z from "zod";
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
+import { Card, CardContent } from "@/components/ui/card";
+import { ArrowLeft } from "lucide-react";
 
 export const resetPasswordSchema = z.object({
 	email: z.string().email({ message: "Invalid email address" }),
@@ -35,26 +37,42 @@ const PasswordResetComponent = () => {
 	};
 
 	return (
-		<div className="flex w-full items-center justify-center py-12">
-			<div className="mx-auto w-[350px]">
-				<h1 className="text-center text-3xl font-bold">Reset Password</h1>
-				<div className="mt-4">
-					<form onSubmit={handleSubmit(handlePasswordReset)}>
-						<Input
-							type="email"
-							placeholder="Enter your email"
-							className="dark:text-gray-950"
-							{...register("email")}
-						/>
-						{errors.email && <span className="text-sm text-red-500">{errors.email.message}</span>}
-						<Button type="submit" className="mt-4 w-full">
-							Send Reset Email
-						</Button>
-					</form>
-					{message && <p className="mt-4 text-center">{message}</p>}
-				</div>
+		<>
+			<div className="flex w-full items-center justify-center py-12">
+				<Card className="mx-auto w-full max-w-[500px]">
+					<CardContent>
+						<div className="max-w-screen-xl px-4 py-12 sm:px-6 lg:px-8">
+							<h1 className="text-center text-3xl font-bold">Reset Password</h1>
+							<div className="mt-4">
+								<form onSubmit={handleSubmit(handlePasswordReset)}>
+									<Input
+										type="email"
+										placeholder="Enter your email"
+										className="dark:text-gray-950"
+										{...register("email")}
+									/>
+									{errors.email && (
+										<span className="text-sm text-red-500">{errors.email.message}</span>
+									)}
+									<Button type="submit" className="mt-4 w-full">
+										Send Reset Email
+									</Button>
+								</form>
+								{message && <p className="mt-4 text-center">{message}</p>}
+							</div>
+						</div>
+					</CardContent>
+				</Card>
 			</div>
-		</div>
+			<div className="flex items-center justify-center pb-12">
+				<a type="button" className="mt-4 w-full text-center" href="/signin">
+					<Button type="button" className="mt-4 max-w-sm">
+						<ArrowLeft className="mr-2 h-4 w-4" />
+						Back to Sign In
+					</Button>
+				</a>
+			</div>
+		</>
 	);
 };
 
