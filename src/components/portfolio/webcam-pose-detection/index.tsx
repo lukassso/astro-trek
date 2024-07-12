5d4ed67ddd4f52e057bc4a2cfcd81ef3187e2f1e
Lukasz <lukasz.zatyka@gmail.com>
2024-07-12 13:27:28 +0200

    Trying to loading model with @teachablemachine/pose

--- src/components/portfolio/webcam-pose-detection/index.tsx
+++ src/components/portfolio/webcam-pose-detection/index.tsx
@@ -1,5 +1,6 @@
-import React, { useEffect, useRef } from "react";
+import React, { useRef, useEffect } from "react";
 import Webcam from "react-webcam";
+import * as tmPose from "@teachablemachine/pose";
 
 const videoConstraints = {
 	width: 540,
@@ -9,6 +10,20 @@ const videoConstraints = {
 const Camera: React.FC = () => {
 	const webcamRef = useRef<Webcam>(null);
 	const canvasRef = useRef<HTMLCanvasElement>(null);
+	const modelRef = useRef<tmPose.CustomPoseNet | null>(null);
+	const maxPredictionsRef = useRef<number>(0);
+
+	useEffect(() => {
+		const loadModel = async () => {
+			const modelURL = "./model.json";
+			const metadataURL = "./metadata.json";
+			modelRef.current = await tmPose.load(modelURL, metadataURL);
+			maxPredictionsRef.current = modelRef.current.getTotalClasses();
+			console.log("Model loaded");
+		};
+
+		loadModel();
+	}, []);
 
 	return (
 		<div className="flex min-h-screen flex-col items-center">
@@ -21,6 +36,8 @@ const Camera: React.FC = () => {
 				<canvas
 					ref={canvasRef}
 					className="canvas-detection z-1 border-1 absolute left-4 top-4 rounded-xl"
+					width={540}
+					height={480}
 				/>
 			</div>
 		</div>
