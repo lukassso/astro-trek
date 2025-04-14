4747e9ebbadd21bc83e83842dd31cb0687723c74
Lukasz <lukasz.zatyka@gmail.com>
2025-04-14 18:50:41 +0200

    fix: Stylistic adjustments

--- src/components/home/tech-stack-section/technologies.ts
+++ src/components/home/tech-stack-section/technologies.ts
@@ -6,14 +6,35 @@ type TechArrayProp = {
 
 export const technologies: TechArrayProp[] = [
   // ⚛️ Frameworks/libraries
-  { name: "React", image: "/src/assets/tech-stack/React.svg", category: "Frameworks & Libraries" },
-  { name: "Vue.js", image: "/src/assets/tech-stack/Vue.js.svg", category: "Frameworks & Libraries" },
-  { name: "Astro", image: "/src/assets/tech-stack/Astro.svg", category: "Frameworks & Libraries" },
-  { name: "Gatsby", image: "/src/assets/tech-stack/Gatsby.svg", category: "Frameworks & Libraries" },
-  { name: "Redux", image: "/src/assets/tech-stack/Redux.svg", category: "Frameworks & Libraries" },
-  { name: "Nest.js", image: "/src/assets/tech-stack/Nest.js.svg", category: "Frameworks & Libraries" },
-  { name: "Drupal", image: "/src/assets/tech-stack/Drupal.svg", category: "Frameworks & Libraries" },
-  { name: "WordPress", image: "/src/assets/tech-stack/WordPress.svg", category: "Frameworks & Libraries" },
+  { name: "React.js", image: "/src/assets/tech-stack/React.svg", category: "Frameworks & Libraries" },
+  { name: "Next.js", image: "/src/assets/tech-stack/Next.js.svg", category: "Frameworks & Libraries" },
+
+  {
+    name: "Vue.js",
+    image: "/src/assets/tech-stack/Vue.js.svg",
+    category: "Frameworks & Libraries",
+  },
+  { name: "Astro.js", image: "/src/assets/tech-stack/Astro.svg", category: "Frameworks & Libraries" },
+  {
+    name: "Gatsby.js",
+    image: "/src/assets/tech-stack/Gatsby.svg",
+    category: "Frameworks & Libraries",
+  },
+  {
+    name: "Nest.js",
+    image: "/src/assets/tech-stack/Nest.js.svg",
+    category: "Frameworks & Libraries",
+  },
+  {
+    name: "Drupal",
+    image: "/src/assets/tech-stack/Drupal.svg",
+    category: "Frameworks & Libraries",
+  },
+  {
+    name: "WordPress",
+    image: "/src/assets/tech-stack/WordPress.svg",
+    category: "Frameworks & Libraries",
+  },
   // 🧾 Programming leanguages
   { name: "JavaScript", image: "/src/assets/tech-stack/JavaScript.svg", category: "Languages" },
   { name: "TypeScript", image: "/src/assets/tech-stack/TypeScript.svg", category: "Languages" },
@@ -25,23 +46,30 @@ export const technologies: TechArrayProp[] = [
   { name: "Tailwind CSS", image: "/src/assets/tech-stack/Tailwind-CSS.svg", category: "Frontend" },
   { name: "Vite.js", image: "/src/assets/tech-stack/Vite.js.svg", category: "Frontend" },
   { name: "Webpack", image: "/src/assets/tech-stack/Webpack.svg", category: "Frontend" },
-
+  { name: "Redux", image: "/src/assets/tech-stack/Redux.svg", category: "Frontend" },
+  { name: "MobX", image: "/src/assets/tech-stack/MobX.svg", category: "Frontend" },
+  { name: "Markdown", image: "/src/assets/tech-stack/Markdown.svg", category: "Frontend" },
   // 💻 Backend & API
   { name: "Node.js", image: "/src/assets/tech-stack/Node.js.svg", category: "Backend & API" },
   { name: "GraphQL", image: "/src/assets/tech-stack/GraphQL.svg", category: "Backend & API" },
   { name: "Firebase", image: "/src/assets/tech-stack/Firebase.svg", category: "Backend & API" },
+  { name: "Google Cloud", image: "/src/assets/tech-stack/Google-Cloud.svg", category: "Backend & API" },
 
   // 🧠 Data Science & ML
   { name: "NumPy", image: "/src/assets/tech-stack/NumPy.svg", category: "Data Science & ML" },
   { name: "Pandas", image: "/src/assets/tech-stack/Pandas.svg", category: "Data Science & ML" },
-  { name: "TensorFlow", image: "/src/assets/tech-stack/TensorFlow.svg", category: "Data Science & ML" },
-
+  {
+    name: "TensorFlow",
+    image: "/src/assets/tech-stack/TensorFlow.svg",
+    category: "Data Science & ML",
+  },
   // 🧪 Tests
   { name: "Playwrite", image: "/src/assets/tech-stack/Playwrite.svg", category: "Testing" },
   { name: "Storybook", image: "/src/assets/tech-stack/Storybook.svg", category: "Testing" },
-
-   // 🎨 Design
-   { name: "Figma", image: "/src/assets/tech-stack/Figma.svg", category: "Design" },
-
+  { name: "Jest", image: "/src/assets/tech-stack/Jest.svg", category: "Testing" },
+  // 🎨 Design
+  { name: "Figma", image: "/src/assets/tech-stack/Figma.svg", category: "Design" },
   // 🛢️ Data Bases
-]
+  { name: "Postgresql", image: "/src/assets/tech-stack/PostgresSQL.svg", category: "Data Bases" },
+  { name: "MongoDB", image: "/src/assets/tech-stack/MongoDB.svg", category: "Data Bases" },
+];
